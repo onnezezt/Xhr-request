62be963e03daa9e04df00d287b4189ce449afdce
shunshun <lwwtotod@gmail.com>
2019-06-19 15:18:28 +0800

    feat(处理请求 header):

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { AxiosRequestConfig } from './types'
 import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 import { bulidURL } from './helpers/url'
 import xhr from './xhr'
 
@@ -7,8 +8,10 @@ function axios(config: AxiosRequestConfig) {
   processConfig(config)
   xhr(config)
 }
+
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  config.headers = transformHeaders(config)
   config.data = transformRequestData(config)
 }
 
@@ -21,4 +24,8 @@ function transformRequestData(config: AxiosRequestConfig): any {
   return transformRequest(config.data)
 }
 
+function transformHeaders(config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
+}
 export default axios
